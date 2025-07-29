a93616128df19482c9ee87448768a03dd07b4d1b
Han Yi <hanyiwindows@live.com>
2025-07-29 10:42:25 +0800

    fix(calendar): update deps

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import CalendarView from './CalendarView.tsx';
 import App from './App.tsx';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -13,5 +16,13 @@ root.render(
       <CssBaseline />
       <App />
     </ThemeProvider>
+  </React.StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <CalendarView />
+    </LocalizationProvider>
   </React.StrictMode>
 );
